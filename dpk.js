9bdbfd01ae97c6dfc2eac1a327ec9c894c8ca14e
Prasad Lakmal <prasadlakmal@rocketmail.com>
2023-04-10 11:31:06 +0530

    refactored function and unit tests

--- dpk.js
+++ dpk.js
@@ -1,28 +1,59 @@
 const crypto = require("crypto");
 
+const MAX_PARTITION_KEY_LENGTH = 256;
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+  if (!event.partitionKey) {
+    const stringifiedEvent = JSON.stringify(event);
+    // This function will always return a 128 length string so no need to validate it's length.
+    return createHash(stringifiedEvent);
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+
+  return lengthValidatedKey(event.partitionKey);
 };
+
+function createHash(data) {
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
+
+function lengthValidatedKey(key) {
+  const stringifiedKey = typeof key === 'string' ? key : JSON.stringify(key);
+
+  if (stringifiedKey.length > MAX_PARTITION_KEY_LENGTH) {
+    return createHash(stringifiedKey);
+  }
+
+  return stringifiedKey;
+}
+
+// exports.deterministicPartitionKeyOld = (event) => {
+//   const TRIVIAL_PARTITION_KEY = "0";
+//   let candidate;
+
+//   if (event) {
+//     if (event.partitionKey) {
+//       candidate = event.partitionKey;
+//     } else {
+//       const data = JSON.stringify(event);
+//       candidate = createHash(data);
+//     }
+//   }
+
+//   if (candidate) { // This if is always true when `event` is truthy
+//     if (typeof candidate !== "string") {
+//       candidate = JSON.stringify(candidate);
+//     }
+//   } else {
+//     candidate = TRIVIAL_PARTITION_KEY; // If event is falsy this value could have returned straight away, because this is the shortast path.
+//   }
+//   if (candidate.length > MAX_PARTITION_KEY_LENGTH) { // This seems to be a mandatory validation in all cases
+//     candidate = createHash(candidate);
+//   }
+//   return candidate;
+// };
--- dpk.test.js
+++ dpk.test.js
@@ -1,8 +1,40 @@
 const { deterministicPartitionKey } = require("./dpk");
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
-    const trivialKey = deterministicPartitionKey();
+  it("Returns the literal '0' when event is falsy", () => {
+    const mockEvent = undefined;
+    // if someone refactors the function adding a default param value the test will be invalid. So passng `undefined` explicitly.
+    const trivialKey = deterministicPartitionKey(mockEvent);
     expect(trivialKey).toBe("0");
+  });
+
+  it("Returns a valid key when partitionKey is falsy", () => {
+    const mockEvent = { partitionKey: undefined };
+    const trivialKey = deterministicPartitionKey(mockEvent);
+    expect(trivialKey).toBe("c1802e6b9670927ebfddb7f67b3824642237361f07db35526c42c555ffd2dbe74156c366e1550ef8c0508a6cc796409a7194a59bba4d300a6182b483d315a862");
+  });
+
+  it("Returns a valid key when partitionKey is truthy and length < 256", () => {
+    const mockEvent = { partitionKey: "1".repeat(255) };
+    const trivialKey = deterministicPartitionKey(mockEvent);
+    expect(trivialKey).toBe(mockEvent.partitionKey);
+  });
+
+  it("Returns a valid key when partitionKey is truthy and length = 256", () => {
+    const mockEvent = { partitionKey: "1".repeat(256) };
+    const trivialKey = deterministicPartitionKey(mockEvent);
+    expect(trivialKey).toBe(mockEvent.partitionKey);
+  });
+
+  it("Returns a valid key when partitionKey is truthy and length > 256", () => {
+    const mockEvent = { partitionKey: "1".repeat(257) };
+    const trivialKey = deterministicPartitionKey(mockEvent);
+    expect(trivialKey).toBe("3f2e417dd3287bb9d5a0e47a8a25191210abdd7739d882cea800f3180dc91508c047c737c51abad48d4d4f2469776294e2b4d9de0af65bffb147d7655ff49fa8");
+  });
+
+  it("Returns a valid key when partitionKey is not a string", () => {
+    const mockEvent = { partitionKey: 11111111 };
+    const trivialKey = deterministicPartitionKey(mockEvent);
+    expect(trivialKey).toBe(String(mockEvent.partitionKey));
   });
 });
